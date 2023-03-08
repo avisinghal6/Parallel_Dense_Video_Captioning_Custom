0e5def06f57ecd1e8f5dd6b6681e032097ec6ff8
avisinghal6 <avisinghal6@gmail.com>
2023-03-08 17:50:07 -0600

    added DETR to submodules

--- detr.sh
+++ detr.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -i
 curdir=`pwd`
-git clone https://github.com/fundamentalvision/Deformable-DETR.git
 cd Deformable-DETR/
 pip install -r requirements.txt
-cd /content/Parallel_Dense_Video_Captioning_Custom/Deformable-DETR/models/ops
+cd models/ops
 python setup.py install
